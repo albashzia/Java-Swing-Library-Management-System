a6a3974a5e09e1aae16aac0eb43612801de1a1e4
albashzia <ahmedalbashzia@gmail.com>
2026-03-18 00:49:03 +0500

    Making necessary import in MembersHomeFrame class, creating a constructor, creating a frame object and defining basic properties of the frame

--- src/MembersHomeFrame.java
+++ src/MembersHomeFrame.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class MembersHomeFrame {
+    JFrame mhFrame = new JFrame();
+
+    MembersHomeFrame(){
+        mhFrame.setSize(800,600);
+        mhFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mhFrame.setLayout(null);
+        mhFrame.setLocationRelativeTo(null);
+        mhFrame.setTitle("Membership Management");
+
+        mhFrame.setVisible(true);
+    }
 }
