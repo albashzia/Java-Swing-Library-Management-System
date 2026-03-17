139b6a7021e57683110fdd6a8e59179d15c8d885
albashzia <ahmedalbashzia@gmail.com>
2026-03-18 01:27:51 +0500

    Creating a JButton object in RoomsHomeFrame class

--- src/RoomsHomeFrame.java
+++ src/RoomsHomeFrame.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class RoomsHomeFrame {
     
     JFrame srhFrame = new JFrame();
+    JButton backButton = new JButton();
+
     RoomsHomeFrame(){
         srhFrame.setSize(800,600);
         srhFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
