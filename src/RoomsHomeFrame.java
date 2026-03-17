cdc76509554a811f6c235da378c7bac12f9c80e6
albashzia <ahmedalbashzia@gmail.com>
2026-03-18 01:28:52 +0500

    Defining the basic methods to create a button and adding the button to the frame

--- src/RoomsHomeFrame.java
+++ src/RoomsHomeFrame.java
@@ -12,6 +12,12 @@ public class RoomsHomeFrame {
         srhFrame.setLocationRelativeTo(null);
         srhFrame.setTitle("Study Room Management");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+        srhFrame.add(backButton);
         srhFrame.setVisible(true);
     }
 }
