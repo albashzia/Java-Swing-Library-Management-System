8e7959abf09c325f673a1f75b6b7e82df6052442
albashzia <ahmedalbashzia@gmail.com>
2026-03-17 21:43:15 +0500

    Adding method to make the frame appear in center of the display

--- src/HomeFrame.java
+++ src/HomeFrame.java
@@ -8,6 +8,7 @@ public class HomeFrame {
         homeFrame.setSize(800,600);
         homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         homeFrame.setLayout(null);
+        homeFrame.setLocationRelativeTo(null);
         homeFrame.setTitle("Library Management System");
         homeFrame.setVisible(true);
     }
