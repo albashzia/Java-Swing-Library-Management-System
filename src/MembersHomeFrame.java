e8d7ae4885541114749ae8c7837a4abd2cc931f5
albashzia <ahmedalbashzia@gmail.com>
2026-03-18 00:53:16 +0500

    Creating a JButton object, defining its properties and adding it to the frame

--- src/MembersHomeFrame.java
+++ src/MembersHomeFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class MembersHomeFrame {
     JFrame mhFrame = new JFrame();
+    JButton backButton = new JButton();
 
     MembersHomeFrame(){
         mhFrame.setSize(800,600);
@@ -10,6 +11,12 @@ public class MembersHomeFrame {
         mhFrame.setLocationRelativeTo(null);
         mhFrame.setTitle("Membership Management");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+        mhFrame.add(backButton);
         mhFrame.setVisible(true);
     }
 }
