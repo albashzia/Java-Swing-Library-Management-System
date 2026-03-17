0f26726eb5c5562ce4fab274fc333b75278527c6
albashzia <ahmedalbashzia@gmail.com>
2026-03-18 01:23:40 +0500

    Creating a JFrame object and defining basic properties of the frame object created

--- src/RoomsHomeFrame.java
+++ src/RoomsHomeFrame.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 
 public class RoomsHomeFrame {
-
+    
+    JFrame srhFrame = new JFrame();
     RoomsHomeFrame(){
-
+        srhFrame.setSize(800,600);
+        srhFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        srhFrame.setLayout(null);
+        srhFrame.setLocationRelativeTo(null);
+        srhFrame.setTitle("Study Room Management");
     }
 }
