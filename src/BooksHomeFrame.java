d4079bd1ce9a1d596218d1d225e770ee968af6b3
albashzia <ahmedalbashzia@gmail.com>
2026-03-18 16:01:21 +0500

    Defining the properties for the label on BooksHomeFrame, adding text, background color etc and adding it to the frame

--- src/BooksHomeFrame.java
+++ src/BooksHomeFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,12 +15,23 @@ public class BooksHomeFrame implements ActionListener {
         bhFrame.setLocationRelativeTo(null);
         bhFrame.setTitle("Books Management");
 
+        label1.setLocation(200,30);
+        label1.setSize(400,70);
+        label1.setBackground(Color.BLACK);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        label1.setVerticalAlignment(JLabel.CENTER);
+        label1.setText("Books Management");
+        label1.setFont(new Font("Comic Sans",Font.ITALIC,35));
+        label1.setForeground(Color.WHITE);
+        label1.setOpaque(true);
+
         backButton.setLocation(30,30);
         backButton.setSize(80,40);
         backButton.setText("Back");
         backButton.setFocusable(false);
         backButton.addActionListener(this);
 
+        bhFrame.add(label1);
         bhFrame.add(backButton);
         bhFrame.setVisible(true);
     }
