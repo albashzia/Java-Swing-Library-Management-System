2534ba398cce0ae7e21840cfd1b29103d43ec179
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:50:46 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in SearchBookFrame class

--- src/SearchBookFrame.java
+++ src/SearchBookFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class SearchBookFrame {
+
+    JFrame sbFrame = new JFrame();
+
+    SearchBookFrame(){
+
+        sbFrame.setSize(800,600);
+        sbFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sbFrame.setLayout(null);
+        sbFrame.setLocationRelativeTo(null);
+        sbFrame.setTitle("Search Book");
+
+        sbFrame.setVisible(true);
+    }
 }
