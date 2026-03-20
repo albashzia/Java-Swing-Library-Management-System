fb31df8f957c1e1cd62d7f0fa1a6fdc1b034db2d
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:53:51 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in IssueBookFrame class

--- src/IssueBookFrame.java
+++ src/IssueBookFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class IssueBookFrame {
+
+    JFrame ibFrame = new JFrame();
+
+    IssueBookFrame(){
+
+        ibFrame.setSize(800,600);
+        ibFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ibFrame.setLayout(null);
+        ibFrame.setLocationRelativeTo(null);
+        ibFrame.setTitle("Issue Book");
+
+        ibFrame.setVisible(true);
+    }
 }
