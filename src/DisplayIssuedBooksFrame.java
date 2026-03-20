0401ddf4d2a1562fcf5692a511c01972f68ba8f2
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:57:30 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in DisplayIssuedBooksFrame class

--- src/DisplayIssuedBooksFrame.java
+++ src/DisplayIssuedBooksFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class DisplayIssuedBooksFrame {
+
+    JFrame dibFrame = new JFrame();
+
+    DisplayIssuedBooksFrame(){
+
+        dibFrame.setSize(800,600);
+        dibFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dibFrame.setLayout(null);
+        dibFrame.setLocationRelativeTo(null);
+        dibFrame.setTitle("Display Issued Books");
+
+        dibFrame.setVisible(true);
+    }
 }
