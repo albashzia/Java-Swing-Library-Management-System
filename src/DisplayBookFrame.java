b47e20dc2fe379736ba425d199b501a0663b4f91
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:52:15 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in DisplayBookFrame class

--- src/DisplayBookFrame.java
+++ src/DisplayBookFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class DisplayBookFrame {
+
+    JFrame dbFrame = new JFrame();
+
+    DisplayBookFrame(){
+
+        dbFrame.setSize(800,600);
+        dbFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dbFrame.setLayout(null);
+        dbFrame.setLocationRelativeTo(null);
+        dbFrame.setTitle("Display Book");
+
+        dbFrame.setVisible(true);
+    }
 }
