cb459df48e4081454f2eacae9d73a6b6585f81f5
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:55:19 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in IssueBookFrame class

--- src/ReturnBookFrame.java
+++ src/ReturnBookFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class ReturnBookFrame {
+
+    JFrame rbFrame = new JFrame();
+
+    ReturnBookFrame(){
+
+        rbFrame.setSize(800,600);
+        rbFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rbFrame.setLayout(null);
+        rbFrame.setLocationRelativeTo(null);
+        rbFrame.setTitle("Return Book");
+
+        rbFrame.setVisible(true);
+    }
 }
