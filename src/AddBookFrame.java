95a64378d64210fb59c2c780e412427e52fc9796
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:44:27 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in AddBookFrame class

--- src/AddBookFrame.java
+++ src/AddBookFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class AddBookFrame {
+    
+    JFrame abFrame = new JFrame();
+    
+    AddBookFrame(){
+
+        abFrame.setSize(800,600);
+        abFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        abFrame.setLayout(null);
+        abFrame.setLocationRelativeTo(null);
+        abFrame.setTitle("Add Book");
+
+        abFrame.setVisible(true);
+    }
 }
