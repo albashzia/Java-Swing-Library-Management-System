c49e1472100ba2f40c30f73078bfc31bcd0898cb
albashzia <ahmedalbashzia@gmail.com>
2026-03-20 14:48:52 +0500

    Adding necessary import, creating a JFrame object, defining a constructor and basic properties of a frame in RemoveBookFrame class

--- src/RemoveBookFrame.java
+++ src/RemoveBookFrame.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class RemoveBookFrame {
+
+    JFrame rbFrame = new JFrame();
+
+    RemoveBookFrame(){
+
+        rbFrame.setSize(800,600);
+        rbFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rbFrame.setLayout(null);
+        rbFrame.setLocationRelativeTo(null);
+        rbFrame.setTitle("Remove Book");
+
+        rbFrame.setVisible(true);
+    }
 }
