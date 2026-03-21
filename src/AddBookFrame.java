f966ec401bb1a95abba47964a06f882804f836e0
albashzia <ahmedalbashzia@gmail.com>
2026-03-21 23:06:34 +0500

    Creating a JButton object in AddBookFrame class, defining its basic attributes and adding it to the frame.

--- src/AddBookFrame.java
+++ src/AddBookFrame.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 public class AddBookFrame {
     
     JFrame abFrame = new JFrame();
-    
+    JButton backButton = new JButton();
+
     AddBookFrame(){
 
         abFrame.setSize(800,600);
@@ -12,6 +13,14 @@ public class AddBookFrame {
         abFrame.setLocationRelativeTo(null);
         abFrame.setTitle("Add Book");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+
+
+        abFrame.add(backButton);
         abFrame.setVisible(true);
     }
 }
