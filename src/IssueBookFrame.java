f977520a8f0ef171042c026d45ac1ba5dc3025a2
albashzia <ahmedalbashzia@gmail.com>
2026-03-21 23:23:06 +0500

    Creating a JButton object in IssueBookFrame class, defining its basic attributes and adding it to the frame.

--- src/IssueBookFrame.java
+++ src/IssueBookFrame.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class IssueBookFrame {
 
     JFrame ibFrame = new JFrame();
+    JButton backButton = new JButton();
 
     IssueBookFrame(){
 
@@ -12,6 +13,12 @@ public class IssueBookFrame {
         ibFrame.setLocationRelativeTo(null);
         ibFrame.setTitle("Issue Book");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+        ibFrame.add(backButton);
         ibFrame.setVisible(true);
     }
 }
