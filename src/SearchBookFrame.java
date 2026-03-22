e3d3829a990ee9418ddf839190d4678b5dbe5073
albashzia <ahmedalbashzia@gmail.com>
2026-03-22 20:47:34 +0500

    Creating a JLabel object in SearchBookFrame class to display the label on the frame with specified text.

--- src/SearchBookFrame.java
+++ src/SearchBookFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,6 +7,8 @@ public class SearchBookFrame implements ActionListener {
 
     JFrame sbFrame = new JFrame();
     JButton backButton = new JButton();
+    JLabel label1 = new JLabel();
+
     SearchBookFrame(){
 
         sbFrame.setSize(800,600);
@@ -20,6 +23,17 @@ public class SearchBookFrame implements ActionListener {
         backButton.setFocusable(false);
         backButton.addActionListener(this);
 
+        label1.setLocation(200,30);
+        label1.setSize(400,70);
+        label1.setBackground(Color.BLACK);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        label1.setVerticalAlignment(JLabel.CENTER);
+        label1.setText("Add a Book");
+        label1.setFont(new Font("Comic Sans",Font.ITALIC,35));
+        label1.setForeground(Color.WHITE);
+        label1.setOpaque(true);
+
+        sbFrame.add(label1);
         sbFrame.add(backButton);
         sbFrame.setVisible(true);
     }
