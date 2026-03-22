53b3bd1a22b3b7c81bf663dbff36bed0cd268d5a
albashzia <ahmedalbashzia@gmail.com>
2026-03-22 20:56:27 +0500

    Creating a JLabel object  ReturnBookFrame class to display the label on the frame with specified text.

--- src/ReturnBookFrame.java
+++ src/ReturnBookFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,6 +7,7 @@ public class ReturnBookFrame implements ActionListener {
 
     JFrame rbFrame = new JFrame();
     JButton backButton = new JButton();
+    JLabel label1 = new JLabel();
 
     ReturnBookFrame(){
 
@@ -21,6 +23,17 @@ public class ReturnBookFrame implements ActionListener {
         backButton.setFocusable(false);
         backButton.addActionListener(this);
 
+        label1.setLocation(200,30);
+        label1.setSize(400,70);
+        label1.setBackground(Color.BLACK);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        label1.setVerticalAlignment(JLabel.CENTER);
+        label1.setText("Return a Book");
+        label1.setFont(new Font("Comic Sans",Font.ITALIC,35));
+        label1.setForeground(Color.WHITE);
+        label1.setOpaque(true);
+
+        rbFrame.add(label1);
         rbFrame.add(backButton);
         rbFrame.setVisible(true);
     }
