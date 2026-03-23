444a6331cf6add44d77d1cd33e07bddebf908568
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 22:35:02 +0500

    Defining the attributes for the JFrame object to make the frame visible.

--- src/SearchMemberFrame.java
+++ src/SearchMemberFrame.java
@@ -4,5 +4,12 @@ public class SearchMemberFrame {
     JFrame smFrame = new JFrame();
     SearchMemberFrame(){
 
+        smFrame.setSize(800,600);
+        smFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        smFrame.setLayout(null);
+        smFrame.setLocationRelativeTo(null);
+        smFrame.setTitle("Display Members");
+
+        smFrame.setVisible(true);
     }
 }
