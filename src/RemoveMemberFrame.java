40f3a627ff856238a8b7872877c7308f15572b18
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 23:06:42 +0500

    Adding a backButton to RemoveMemberFrame.java file

--- src/RemoveMemberFrame.java
+++ src/RemoveMemberFrame.java
@@ -2,14 +2,22 @@ import javax.swing.*;
 
 public class RemoveMemberFrame {
     JFrame rmFrame = new JFrame();
+    JButton backButton = new JButton();
+
     RemoveMemberFrame(){
 
         rmFrame.setSize(800,600);
         rmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         rmFrame.setLayout(null);
         rmFrame.setLocationRelativeTo(null);
-        rmFrame.setTitle("Search Members");
+        rmFrame.setTitle("Remove Member");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+        rmFrame.add(backButton);
         rmFrame.setVisible(true);
         
     }
