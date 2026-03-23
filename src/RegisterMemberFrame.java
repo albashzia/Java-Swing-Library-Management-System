cdbee64ca488f02dcb44c5d6292565c007fcf3c1
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 19:51:58 +0500

    Defining the attributes for the JFrame object to make the frame visible for RegisterMemberFrame class.

--- src/RegisterMemberFrame.java
+++ src/RegisterMemberFrame.java
@@ -4,6 +4,15 @@ public class RegisterMemberFrame {
     JFrame rmFrame = new JFrame();
 
     RegisterMemberFrame(){
+
+        rmFrame.setSize(800,600);
+        rmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rmFrame.setLayout(null);
+        rmFrame.setLocationRelativeTo(null);
+        rmFrame.setTitle("Register a Member");
+
+
+        rmFrame.setVisible(true);
         
     }
 }
