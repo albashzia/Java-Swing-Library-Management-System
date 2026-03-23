7f13fca5f85c8aa9158b8307ae2b2132c3f0f36c
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 23:04:54 +0500

    Adding a backButton to DisplayMembersFrame.java file

--- src/DisplayMembersFrame.java
+++ src/DisplayMembersFrame.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 
 public class DisplayMembersFrame {
     JFrame dmFrame = new JFrame();
+    JButton backButton = new JButton();
+
     DisplayMembersFrame(){
 
         dmFrame.setSize(800,600);
@@ -10,6 +12,12 @@ public class DisplayMembersFrame {
         dmFrame.setLocationRelativeTo(null);
         dmFrame.setTitle("Display Members");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+        dmFrame.add(backButton);
         dmFrame.setVisible(true);
     }
 }
