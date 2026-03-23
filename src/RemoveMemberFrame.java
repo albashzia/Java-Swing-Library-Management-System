700f286ed64d6f54a6fc225612ea82e4e5cb67c6
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 22:54:30 +0500

    Defining the attributes for the JFrame object to make the frame visible.

--- src/RemoveMemberFrame.java
+++ src/RemoveMemberFrame.java
@@ -4,5 +4,13 @@ public class RemoveMemberFrame {
     JFrame rmFrame = new JFrame();
     RemoveMemberFrame(){
 
+        rmFrame.setSize(800,600);
+        rmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rmFrame.setLayout(null);
+        rmFrame.setLocationRelativeTo(null);
+        rmFrame.setTitle("Search Members");
+
+        rmFrame.setVisible(true);
+        
     }
 }
