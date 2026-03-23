61b6454e282e8826120480d08cda625114d66ba1
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 22:50:10 +0500

    Defining the attributes for the JFrame object to make the frame visible.

--- src/IssuedToMemberFrame.java
+++ src/IssuedToMemberFrame.java
@@ -4,5 +4,12 @@ public class IssuedToMemberFrame {
     JFrame itmFrame = new JFrame();
     IssuedToMemberFrame(){
 
+        itmFrame.setSize(800,600);
+        itmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        itmFrame.setLayout(null);
+        itmFrame.setLocationRelativeTo(null);
+        itmFrame.setTitle("Book Issued to Member");
+
+        itmFrame.setVisible(true);
     }
 }
