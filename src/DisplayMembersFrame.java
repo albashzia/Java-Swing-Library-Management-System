50b2f79f60da6e20d56ec6ae89d6e63be349f021
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 22:26:53 +0500

    Defining the attributes for the JFrame object to make the frame visible.

--- src/DisplayMembersFrame.java
+++ src/DisplayMembersFrame.java
@@ -4,5 +4,12 @@ public class DisplayMembersFrame {
     JFrame dmFrame = new JFrame();
     DisplayMembersFrame(){
 
+        dmFrame.setSize(800,600);
+        dmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dmFrame.setLayout(null);
+        dmFrame.setLocationRelativeTo(null);
+        dmFrame.setTitle("Display Members");
+
+        dmFrame.setVisible(true);
     }
 }
