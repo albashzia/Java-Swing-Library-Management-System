61a8362b5276c395cea900c712fc1e8325d2300a
albashzia <ahmedalbashzia@gmail.com>
2026-03-23 23:07:22 +0500

    Adding a backButton to SearchMemberFrame.java file

--- src/SearchMemberFrame.java
+++ src/SearchMemberFrame.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 
 public class SearchMemberFrame {
     JFrame smFrame = new JFrame();
+    JButton backButton = new JButton();
+
     SearchMemberFrame(){
 
         smFrame.setSize(800,600);
@@ -10,6 +12,12 @@ public class SearchMemberFrame {
         smFrame.setLocationRelativeTo(null);
         smFrame.setTitle("Search Members");
 
+        backButton.setLocation(30,30);
+        backButton.setSize(80,40);
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+
+        smFrame.add(backButton);
         smFrame.setVisible(true);
     }
 }
