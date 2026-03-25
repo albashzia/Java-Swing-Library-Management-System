89d476924c83c2b762523ac08495d4c1dbe2fc59
albashzia <ahmedalbashzia@gmail.com>
2026-03-25 15:42:59 +0500

    Describing the basic attributes of the frame in CheckRoomStatusFrame constructor to make the frame visible

--- src/GUI/RoomFrames/CheckRoomStatusFrame.java
+++ src/GUI/RoomFrames/CheckRoomStatusFrame.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 public class CheckRoomStatusFrame {
     JFrame crsFrame = new JFrame();
     public CheckRoomStatusFrame(){
+        crsFrame.setSize(800,600);
+        crsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        crsFrame.setLayout(null);
+        crsFrame.setLocationRelativeTo(null);
+        crsFrame.setTitle("Show Available Rooms");
 
+        crsFrame.setVisible(true);
     }
 }
