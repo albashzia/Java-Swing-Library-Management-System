e6d32bc2b0696daadcf5a4abfd5609dc1f380beb
albashzia <ahmedalbashzia@gmail.com>
2026-03-25 15:44:32 +0500

    Describing the basic attributes of the frame in ReserveRoomFrame constructor to make the frame visible

--- src/GUI/RoomFrames/ReserveRoomFrame.java
+++ src/GUI/RoomFrames/ReserveRoomFrame.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 public class ReserveRoomFrame {
     JFrame rrFrame = new JFrame();
     public ReserveRoomFrame(){
+        rrFrame.setSize(800,600);
+        rrFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        rrFrame.setLayout(null);
+        rrFrame.setLocationRelativeTo(null);
+        rrFrame.setTitle("Reserve Study Room");
 
+        rrFrame.setVisible(true);
     }
 }
