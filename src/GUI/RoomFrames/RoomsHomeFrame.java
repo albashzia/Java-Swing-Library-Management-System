1fe005a23de0b8d04bc79f7f1cfa32caddb67ea0
albashzia <ahmedalbashzia@gmail.com>
2026-03-25 15:52:46 +0500

    Adding body to actionPerformed method in RoomsHomeFrame.java and adding action listener method to the buttons

--- src/GUI/RoomFrames/RoomsHomeFrame.java
+++ src/GUI/RoomFrames/RoomsHomeFrame.java
@@ -55,6 +55,11 @@ public class RoomsHomeFrame implements ActionListener {
         button3.setFocusable(false);
         button4.setFocusable(false);
 
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+        button4.addActionListener(this);
+
         srhFrame.add(button1);
         srhFrame.add(button2);
         srhFrame.add(button3);
@@ -68,6 +73,22 @@ public class RoomsHomeFrame implements ActionListener {
         if (e.getSource()==backButton){
             srhFrame.dispose();
             new HomeFrame();
+        }
+        if (e.getSource()==button1){
+            srhFrame.dispose();
+            new ShowRoomsFrame();
+        }
+        if (e.getSource()==button2){
+            srhFrame.dispose();
+            new ReserveRoomFrame();
+        }
+        if (e.getSource()==button3){
+            srhFrame.dispose();
+            new CancelReservationFrame();
+        }
+        if (e.getSource()==button4){
+            srhFrame.dispose();
+            new CheckRoomStatusFrame();
         }
     }
 }
