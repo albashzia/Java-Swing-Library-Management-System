5bfc75269817c3344f1c24a88e3332e364c4a91a
albashzia <ahmedalbashzia@gmail.com>
2026-03-25 15:41:37 +0500

    Describing the basic attributes of the frame in CancelReservationFrame constructor to make the frame visible

--- src/GUI/RoomFrames/CancelReservationFrame.java
+++ src/GUI/RoomFrames/CancelReservationFrame.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 public class CancelReservationFrame {
     JFrame crFrame = new JFrame();
     public CancelReservationFrame(){
+        crFrame.setSize(800,600);
+        crFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        crFrame.setLayout(null);
+        crFrame.setLocationRelativeTo(null);
+        crFrame.setTitle("Show Available Rooms");
 
+        crFrame.setVisible(true);
     }
 }
