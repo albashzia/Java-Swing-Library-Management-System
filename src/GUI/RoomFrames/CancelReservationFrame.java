2b9d614d646a075596ced47a70d5d761e1e23e6f
albashzia <ahmedalbashzia@gmail.com>
2026-03-26 12:26:42 +0500

    Adding body to actionPerformed method to dispose the current frame and create and instance of previous frame while also adding actionListener method to the backButton

--- src/GUI/RoomFrames/CancelReservationFrame.java
+++ src/GUI/RoomFrames/CancelReservationFrame.java
@@ -19,12 +19,16 @@ public class CancelReservationFrame implements ActionListener {
         backButton.setSize(80,40);
         backButton.setText("Back");
         backButton.setFocusable(false);
+        backButton.addActionListener(this);
 
         crFrame.add(backButton);
         crFrame.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e){
-        
+        if (e.getSource()==backButton){
+            crFrame.dispose();
+            new RoomsHomeFrame();
+        }
     }
 }
