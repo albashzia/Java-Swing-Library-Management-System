9f253570a811746dcb6e53793ac0f8c410637ca4
albashzia <ahmedalbashzia@gmail.com>
2026-03-27 00:26:15 +0500

    Adding title to RegisterMemberFrame object using CreateLabel object

--- src/GUI/MemberFrames/RegisterMemberFrame.java
+++ src/GUI/MemberFrames/RegisterMemberFrame.java
@@ -1,5 +1,7 @@
 package GUI.MemberFrames;
 
+import GUI.CreateLabel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,6 +9,7 @@ import java.awt.event.ActionListener;
 public class RegisterMemberFrame implements ActionListener {
     JFrame rmFrame = new JFrame();
     JButton backButton = new JButton();
+    CreateLabel label1 = new CreateLabel("Register a Member");
 
     RegisterMemberFrame(){
 
@@ -22,6 +25,7 @@ public class RegisterMemberFrame implements ActionListener {
         backButton.setFocusable(false);
         backButton.addActionListener(this);
 
+        rmFrame.add(label1);
         rmFrame.add(backButton);
         rmFrame.setVisible(true);
         
