4fd20b71fa1a7f651527c4df10fa5b036169524a
albashzia <ahmedalbashzia@gmail.com>
2026-03-26 12:27:51 +0500

    Implementing ActionListener interface and overriding actionPerformed method for CheckRoomStatusFrame class

--- src/GUI/RoomFrames/CheckRoomStatusFrame.java
+++ src/GUI/RoomFrames/CheckRoomStatusFrame.java
@@ -1,8 +1,10 @@
 package GUI.RoomFrames;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CheckRoomStatusFrame {
+public class CheckRoomStatusFrame implements ActionListener {
     JFrame crsFrame = new JFrame();
     JButton backButton = new JButton();
 
@@ -20,5 +22,9 @@ public class CheckRoomStatusFrame {
 
         crsFrame.add(backButton);
         crsFrame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+
     }
 }
