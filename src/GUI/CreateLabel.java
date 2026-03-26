cc96368f0f670d450efabde96ff43f9197c0bf05
albashzia <ahmedalbashzia@gmail.com>
2026-03-26 23:14:44 +0500

    Creating a CreateLabel class to create a title label for all the frames by creating the object of CreateLabel class

---
+++ src/GUI/CreateLabel.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CreateLabel extends JLabel{
+    JLabel label1 = new JLabel();
+
+    public CreateLabel(String title){
+        this.setLocation(200,30);
+        this.setSize(400,70);
+        this.setBackground(Color.BLACK);
+        this.setHorizontalAlignment(JLabel.CENTER);
+        this.setVerticalAlignment(JLabel.CENTER);
+        this.setText(title);
+        this.setFont(new Font("Comic Sans",Font.ITALIC,35));
+        this.setForeground(Color.WHITE);
+        this.setOpaque(true);
+    }
+}
