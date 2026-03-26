5b8c0dd25a9115b729bea2d9a813713cc17ab3f9
albashzia <ahmedalbashzia@gmail.com>
2026-03-26 23:47:14 +0500

    Adding title to CheckRoomStatusFrame object using CreateLabel object

--- src/GUI/RoomFrames/CheckRoomStatusFrame.java
+++ src/GUI/RoomFrames/CheckRoomStatusFrame.java
@@ -1,5 +1,7 @@
 package GUI.RoomFrames;
 
+import GUI.CreateLabel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,7 +9,7 @@ import java.awt.event.ActionListener;
 public class CheckRoomStatusFrame implements ActionListener {
     JFrame crsFrame = new JFrame();
     JButton backButton = new JButton();
-
+    CreateLabel label1 = new CreateLabel("Check Room Status");
     public CheckRoomStatusFrame(){
         crsFrame.setSize(800,600);
         crsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,6 +23,7 @@ public class CheckRoomStatusFrame implements ActionListener {
         backButton.setFocusable(false);
         backButton.addActionListener(this);
 
+        crsFrame.add(label1);
         crsFrame.add(backButton);
         crsFrame.setVisible(true);
     }
