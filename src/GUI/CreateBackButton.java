63283b9c44865a316911e0013e90269dc782801a
albashzia <ahmedalbashzia@gmail.com>
2026-03-28 20:45:21 +0500

    Removing the implementation of Action listener class and overriden method

--- src/GUI/CreateBackButton.java
+++ src/GUI/CreateBackButton.java
@@ -4,17 +4,11 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CreateBackButton extends JButton implements ActionListener {
-    CreateBackButton() {
+public class CreateBackButton extends JButton {
+    public CreateBackButton() {
         this.setLocation(30, 30);
         this.setSize(80, 40);
         this.setText("Back");
         this.setFocusable(false);
-        this.addActionListener(this);
-    }
-
-    //abstract method
-    public void actionPerformed(ActionEvent e){
-
     }
 }
