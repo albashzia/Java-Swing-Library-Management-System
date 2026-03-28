6d61a986c81f80f960a8ffbdb4eb7f383e2e7b7f
albashzia <ahmedalbashzia@gmail.com>
2026-03-28 20:09:36 +0500

    Implementing ActionListener, overriding actionPerformed method and add actionListener method to the object of CreateBackButton class

--- src/GUI/CreateBackButton.java
+++ src/GUI/CreateBackButton.java
@@ -1,12 +1,20 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CreateBackButton extends JButton {
+public class CreateBackButton extends JButton implements ActionListener {
     CreateBackButton() {
         this.setLocation(30, 30);
         this.setSize(80, 40);
         this.setText("Back");
         this.setFocusable(false);
+        this.addActionListener(this);
+    }
+
+    //abstract method
+    public void actionPerformed(ActionEvent e){
+
     }
 }
