b4c82aef1840a7658446f9919dfdee6dfd29bb0d
albashzia <ahmedalbashzia@gmail.com>
2026-03-28 20:53:32 +0500

    Removing the instantiation of JButton object from SearchBookFrame class and instead adding the back button by creating an object of CreateBackButton class

--- src/GUI/BookFrames/SearchBookFrame.java
+++ src/GUI/BookFrames/SearchBookFrame.java
@@ -1,5 +1,6 @@
 package GUI.BookFrames;
 
+import GUI.CreateBackButton;
 import GUI.CreateLabel;
 
 import javax.swing.*;
@@ -10,7 +11,7 @@ import java.awt.event.ActionListener;
 public class SearchBookFrame implements ActionListener {
 
     JFrame sbFrame = new JFrame();
-    JButton backButton = new JButton();
+    CreateBackButton backButton = new CreateBackButton();
     CreateLabel label1 = new CreateLabel("Search a Book");
 
     SearchBookFrame(){
@@ -21,10 +22,6 @@ public class SearchBookFrame implements ActionListener {
         sbFrame.setLocationRelativeTo(null);
         sbFrame.setTitle("Search Book");
 
-        backButton.setLocation(30,30);
-        backButton.setSize(80,40);
-        backButton.setText("Back");
-        backButton.setFocusable(false);
         backButton.addActionListener(this);
 
         sbFrame.add(label1);
