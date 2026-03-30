36cc6331cc6bdc147c1b3c06e4baec2b28ca9656
albashzia <ahmedalbashzia@gmail.com>
2026-03-30 20:32:39 +0500

    Implementing action listener interface, overriding the actionPerformed method and adding the action listener method to the back button

--- src/GUI/BookFrames/SearchBookByTitleFrame.java
+++ src/GUI/BookFrames/SearchBookByTitleFrame.java
@@ -4,11 +4,14 @@ import GUI.CreateBackButton;
 import GUI.CreateLabel;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SearchBookByTitleFrame {
+public class SearchBookByTitleFrame implements ActionListener {
     JFrame sbtFrame = new JFrame();
     CreateBackButton backButton = new CreateBackButton();
     CreateLabel label = new CreateLabel("Search Book By Title");
+
     public SearchBookByTitleFrame(){
 
         sbtFrame.setSize(800,600);
@@ -17,7 +20,15 @@ public class SearchBookByTitleFrame {
         sbtFrame.setLocationRelativeTo(null);
         sbtFrame.setTitle("Search Book By Title");
 
+        backButton.addActionListener(this);
+
+        sbtFrame.add(label);
         sbtFrame.add(backButton);
         sbtFrame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
     }
 }
