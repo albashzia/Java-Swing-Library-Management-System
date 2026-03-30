855add7dcc63fc8b0905444eaa91e5b601a7631f
albashzia <ahmedalbashzia@gmail.com>
2026-03-30 19:44:13 +0500

    Creating a JFrame object and Back Button and a Label using defined classes

--- src/GUI/BookFrames/SearchBookByIdFrame.java
+++ src/GUI/BookFrames/SearchBookByIdFrame.java
@@ -1,4 +1,14 @@
 package GUI.BookFrames;
 
+import GUI.CreateBackButton;
+import GUI.CreateLabel;
+
+import javax.swing.*;
+
 public class SearchBookByIdFrame {
+    JFrame sbiFrame = new JFrame();
+    CreateBackButton backButton = new CreateBackButton();
+    CreateLabel label = new CreateLabel("Search Book by ID");
+
+
 }
