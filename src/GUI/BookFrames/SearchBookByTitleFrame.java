2d951478e87b2d7a186d28e684658aa356972c6c
albashzia <ahmedalbashzia@gmail.com>
2026-03-30 20:29:51 +0500

    Creating a CreateBackButton object and adding it to the frame

--- src/GUI/BookFrames/SearchBookByTitleFrame.java
+++ src/GUI/BookFrames/SearchBookByTitleFrame.java
@@ -1,10 +1,13 @@
 package GUI.BookFrames;
 
+import GUI.CreateBackButton;
+import GUI.CreateLabel;
+
 import javax.swing.*;
 
 public class SearchBookByTitleFrame {
     JFrame sbtFrame = new JFrame();
-
+    CreateBackButton backButton = new CreateBackButton();
     public SearchBookByTitleFrame(){
 
         sbtFrame.setSize(800,600);
@@ -13,6 +16,7 @@ public class SearchBookByTitleFrame {
         sbtFrame.setLocationRelativeTo(null);
         sbtFrame.setTitle("Search Book By Title");
 
+        sbtFrame.add(backButton);
         sbtFrame.setVisible(true);
     }
 }
