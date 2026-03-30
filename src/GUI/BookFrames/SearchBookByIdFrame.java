f141102bb1774e2047cd0616a6974a315ce8a135
albashzia <ahmedalbashzia@gmail.com>
2026-03-30 19:46:11 +0500

    Defining the attributes of the SearchBookByIdFrame and making it visible

--- src/GUI/BookFrames/SearchBookByIdFrame.java
+++ src/GUI/BookFrames/SearchBookByIdFrame.java
@@ -9,6 +9,16 @@ public class SearchBookByIdFrame {
     JFrame sbiFrame = new JFrame();
     CreateBackButton backButton = new CreateBackButton();
     CreateLabel label = new CreateLabel("Search Book by ID");
+    
+    public SearchBookByIdFrame(){
+
+        sbiFrame.setSize(800,600);
+        sbiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sbiFrame.setLayout(null);
+        sbiFrame.setLocationRelativeTo(null);
+        sbiFrame.setTitle("Search Book By ID");
 
 
+        sbiFrame.setVisible(true);
+    }
 }
