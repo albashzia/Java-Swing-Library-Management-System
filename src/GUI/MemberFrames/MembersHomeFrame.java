9906ce35a1386d8917e944b04ead84b0be4eacb3
albashzia <ahmedalbashzia@gmail.com>
2026-03-30 19:19:37 +0500

    Removing the instantiation of JButton object from MembersHomeFrame class and instead adding the back button by creating an object of CreateBackButton class

--- src/GUI/MemberFrames/MembersHomeFrame.java
+++ src/GUI/MemberFrames/MembersHomeFrame.java
@@ -1,5 +1,6 @@
 package GUI.MemberFrames;
 
+import GUI.CreateBackButton;
 import GUI.CreateLabel;
 import GUI.HomeFrame;
 
@@ -10,7 +11,7 @@ import java.awt.event.ActionListener;
 
 public class MembersHomeFrame implements ActionListener {
     JFrame mhFrame = new JFrame();
-    JButton backButton = new JButton();
+    CreateBackButton backButton = new CreateBackButton();
     CreateLabel label1 = new CreateLabel("Membership Management");
     JButton button1 = new JButton();
     JButton button2 = new JButton();
@@ -25,10 +26,6 @@ public class MembersHomeFrame implements ActionListener {
         mhFrame.setLocationRelativeTo(null);
         mhFrame.setTitle("Membership Management");
 
-        backButton.setLocation(30,30);
-        backButton.setSize(80,40);
-        backButton.setText("Back");
-        backButton.setFocusable(false);
         backButton.addActionListener(this);
 
         button1.setBounds(120,130,200,50);
