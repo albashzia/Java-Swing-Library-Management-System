9c1c6289a8b30e2ba6b80fe360038681cbbd139b
albashzia <ahmedalbashzia@gmail.com>
2026-03-30 20:28:55 +0500

    Creating a JFrame object, initializing the constructor, defining the attributes of the frame and making it visible

--- src/GUI/BookFrames/SearchBookByTitleFrame.java
+++ src/GUI/BookFrames/SearchBookByTitleFrame.java
@@ -1,4 +1,18 @@
 package GUI.BookFrames;
 
+import javax.swing.*;
+
 public class SearchBookByTitleFrame {
+    JFrame sbtFrame = new JFrame();
+
+    public SearchBookByTitleFrame(){
+
+        sbtFrame.setSize(800,600);
+        sbtFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sbtFrame.setLayout(null);
+        sbtFrame.setLocationRelativeTo(null);
+        sbtFrame.setTitle("Search Book By Title");
+
+        sbtFrame.setVisible(true);
+    }
 }
