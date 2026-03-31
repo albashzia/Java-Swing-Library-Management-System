c5e850c184d153c1b27bf4d68e41f42537e7d9c1
albashzia <ahmedalbashzia@gmail.com>
2026-03-31 12:17:00 +0500

    Changing the location relative to for TestFrame and adding the CreateExitButton object to it

--- src/GUI/TestFrame.java
+++ src/GUI/TestFrame.java
@@ -4,13 +4,16 @@ import javax.swing.*;
 
 public class TestFrame {
     JFrame frame = new JFrame();
+    CreateExitButton button = new CreateExitButton();
     TestFrame(){
 
         frame.setSize(800,600);
         frame.setTitle("Test Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setLayout(null);
 
+        frame.add(button);
         frame.setVisible(true);
     }
 }
