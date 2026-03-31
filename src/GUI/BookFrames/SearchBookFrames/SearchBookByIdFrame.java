4ee3fc8713d7cbf48cc1a81ea718ad01bd8a6a9f
albashzia <ahmedalbashzia@gmail.com>
2026-03-31 20:15:49 +0500

    Creating an exit button for SearchBookByIdFrame and adding it to the frame

--- src/GUI/BookFrames/SearchBookFrames/SearchBookByIdFrame.java
+++ src/GUI/BookFrames/SearchBookFrames/SearchBookByIdFrame.java
@@ -1,6 +1,7 @@
 package GUI.BookFrames.SearchBookFrames;
 
 import GUI.CreateBackButton;
+import GUI.CreateExitButton;
 import GUI.CreateLabel;
 
 import javax.swing.*;
@@ -10,6 +11,7 @@ import java.awt.event.ActionListener;
 public class SearchBookByIdFrame implements ActionListener {
     JFrame sbiFrame = new JFrame();
     CreateBackButton backButton = new CreateBackButton();
+    CreateExitButton exitButton = new CreateExitButton();
     CreateLabel label = new CreateLabel("Search Book by ID");
     
     public SearchBookByIdFrame(){
@@ -22,6 +24,7 @@ public class SearchBookByIdFrame implements ActionListener {
 
         backButton.addActionListener(this);
 
+        sbiFrame.add(exitButton);
         sbiFrame.add(backButton);
         sbiFrame.add(label);
         sbiFrame.setVisible(true);
