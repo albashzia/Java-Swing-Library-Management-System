ee52f3f6266b1ce7b21a81c542c24515a36fd4d1
albashzia <ahmedalbashzia@gmail.com>
2026-03-31 12:10:12 +0500

    Defining the attributes of a basic frame for TestFrame object

--- src/GUI/TestFrame.java
+++ src/GUI/TestFrame.java
@@ -1,4 +1,16 @@
 package GUI;
 
+import javax.swing.*;
+
 public class TestFrame {
+    JFrame frame = new JFrame();
+    TestFrame(){
+
+        frame.setSize(800,600);
+        frame.setTitle("Test Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        frame.setVisible(true);
+    }
 }
