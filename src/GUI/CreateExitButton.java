63fed88b19fa0a3c7d45a4e3ca899932bd43f034
albashzia <ahmedalbashzia@gmail.com>
2026-03-31 12:11:40 +0500

    Extending JButton, implementing ActionListener interface, defining the attributes of the CreateExitButton object using a constructor and overriding the actionPerformed method to exit the system

--- src/GUI/CreateExitButton.java
+++ src/GUI/CreateExitButton.java
@@ -1,4 +1,20 @@
 package GUI;
 
-public class CreateExitButton {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CreateExitButton extends JButton implements ActionListener {
+    public CreateExitButton(){
+        this.setBounds(100,100,10,10);
+        this.setText("Exit");
+        this.setFocusable(false);
+        this.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource()==this){
+            System.exit(0);
+        }
+    }
 }
