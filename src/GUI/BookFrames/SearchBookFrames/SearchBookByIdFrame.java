c5633cefc18ecf8a5f0a5827679a3ca090c42a81
albashzia <ahmedalbashzia@gmail.com>
2026-04-01 20:55:55 +0500

    Defining the bounds, font, foreground, background and caret color of the text field in SearchBookByIdFrame class and adding it to the frame

--- src/GUI/BookFrames/SearchBookFrames/SearchBookByIdFrame.java
+++ src/GUI/BookFrames/SearchBookFrames/SearchBookByIdFrame.java
@@ -29,9 +29,16 @@ public class SearchBookByIdFrame implements ActionListener {
         idLabel.setBounds(130,200,200,40);
         idLabel.setFont(new Font("Comic Sans",Font.PLAIN,25));
 
+        idTextField.setBounds(350,200,100,40);
+        idTextField.setFont(new Font("Comic",Font.BOLD,20));
+        idTextField.setForeground(Color.WHITE);
+        idTextField.setBackground(Color.BLACK);
+        idTextField.setCaretColor(Color.WHITE);
+
         backButton.addActionListener(this);
 
         sbiFrame.add(idLabel);
+        sbiFrame.add(idTextField);
         sbiFrame.add(exitButton);
         sbiFrame.add(backButton);
         sbiFrame.add(label);
