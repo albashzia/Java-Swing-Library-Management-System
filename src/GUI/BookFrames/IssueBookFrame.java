1b2e8db64dcf8de92bda5531c659b19f923164a7
albashzia <ahmedalbashzia@gmail.com>
2026-04-03 22:20:02 +0500

    Creating two JTextField objects inside IssueBookFrame

--- src/GUI/BookFrames/IssueBookFrame.java
+++ src/GUI/BookFrames/IssueBookFrame.java
@@ -16,6 +16,8 @@ public class IssueBookFrame implements ActionListener {
     CreateExitButton exitButton = new CreateExitButton();
     JLabel idLabel = new JLabel();
     JLabel memberIdLabel = new JLabel();
+    JTextField idTextField = new JTextField();
+    JTextField memberIdTextField = new JTextField();
     CreateLabel label1 = new CreateLabel("Issue a Book");
 
     IssueBookFrame(){
