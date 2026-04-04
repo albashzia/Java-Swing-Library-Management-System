28ce98c3807f0999517312a3e88f96653e5f840c
albashzia <ahmedalbashzia@gmail.com>
2026-04-04 18:33:46 +0500

    Defining three constructors of CreateSubmitButton class with different type of constructors etc required for different purposes

--- src/GUI/CreateSubmitButton.java
+++ src/GUI/CreateSubmitButton.java
@@ -3,5 +3,16 @@ package GUI;
 import javax.swing.*;
 
 public class CreateSubmitButton extends JButton {
+    CreateSubmitButton(String text){
 
+    }
+
+    CreateSubmitButton(int x, int y, String text){
+
+    }
+
+    CreateSubmitButton(int len, int wid, int x, int y, String text){
+
+    }
+    
 }
