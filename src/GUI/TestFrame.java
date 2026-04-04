b6511ed38e8635a6e475c5cdc2d42683b0fa9f80
albashzia <ahmedalbashzia@gmail.com>
2026-04-04 18:43:56 +0500

    Testing the CreateSubmitButton objects added to the TestFrame defined by using all three different types of CreateSubmitButton constructors

--- src/GUI/TestFrame.java
+++ src/GUI/TestFrame.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 public class TestFrame {
     JFrame frame = new JFrame();
     CreateExitButton button = new CreateExitButton();
-    CreateSubmitButton sb = new CreateSubmitButton("Submit");
+    CreateSubmitButton sb1 = new CreateSubmitButton("Submit");
+    CreateSubmitButton sb2 = new CreateSubmitButton(200,200,"Submit");
+    CreateSubmitButton sb3 = new CreateSubmitButton(400,400,100,50,"Submit");
     TestFrame(){
 
         frame.setSize(800,600);
@@ -14,7 +16,9 @@ public class TestFrame {
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
 
-        frame.add(sb);
+        frame.add(sb1);
+        frame.add(sb2);
+        frame.add(sb3);
         frame.add(button);
         frame.setVisible(true);
     }
