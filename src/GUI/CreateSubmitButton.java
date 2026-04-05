4b90909730b4ca34dea7485c36ab671c0d9ce213
albashzia <ahmedalbashzia@gmail.com>
2026-04-05 12:30:30 +0500

    Modifying the access level of CreateSubmitButton constructors, making them public

--- src/GUI/CreateSubmitButton.java
+++ src/GUI/CreateSubmitButton.java
@@ -3,19 +3,19 @@ package GUI;
 import javax.swing.*;
 
 public class CreateSubmitButton extends JButton {
-    CreateSubmitButton(String text){
+    public CreateSubmitButton(String text){
         this.setBounds(350,330,100,50);
         this.setText(text);
         this.setFocusable(false);
     }
 
-    CreateSubmitButton(int x, int y, String text){
+    public CreateSubmitButton(int x, int y, String text){
         this.setBounds(x,y,100,50);
         this.setText(text);
         this.setFocusable(false);
     }
 
-    CreateSubmitButton(int x, int y, int width, int height,  String text){
+    public CreateSubmitButton(int x, int y, int width, int height,  String text){
         this.setBounds(x,y,width,height);
         this.setText(text);
         this.setFocusable(false);
