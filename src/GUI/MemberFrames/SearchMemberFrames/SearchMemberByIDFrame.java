d62e829644d9ff74ab49f36e1103b4b321d26fca
albashzia <ahmedalbashzia@gmail.com>
2026-04-11 11:30:38 +0500

    Defining the basic attributes of SearchMemberByIDFrame and making it visible

--- src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
+++ src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
@@ -7,6 +7,14 @@ public class SearchMemberByIDFrame {
     JFrame smiFrame = new JFrame();
 
     SearchMemberByIDFrame(){
+        smiFrame.setSize(800,600);
+        smiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        smiFrame.setLayout(null);
+        smiFrame.setLocationRelativeTo(null);
+        smiFrame.setTitle("Search Members by ID");
 
+
+
+        smiFrame.setVisible(true);
     }
 }
