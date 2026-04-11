35bd6520039e0ef5ba425a177127b479b99794da
albashzia <ahmedalbashzia@gmail.com>
2026-04-11 11:29:24 +0500

    Defining a JFrame object for SearchMemberByIDFrame

--- src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
+++ src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
@@ -1,6 +1,11 @@
 package GUI.MemberFrames.SearchMemberFrames;
 
+import javax.swing.*;
+
 public class SearchMemberByIDFrame {
+
+    JFrame smiFrame = new JFrame();
+
     SearchMemberByIDFrame(){
 
     }
