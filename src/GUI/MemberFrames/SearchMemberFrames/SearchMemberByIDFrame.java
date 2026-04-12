fcfe882879a4cb813c3d07b6ee24f8af4e320f98
albashzia <ahmedalbashzia@gmail.com>
2026-04-12 18:27:21 +0500

    Creating a label using CreateLabel class and adding it to the SearchMemberByIDFrame

--- src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
+++ src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
@@ -1,10 +1,13 @@
 package GUI.MemberFrames.SearchMemberFrames;
 
+import GUI.CreateLabel;
+
 import javax.swing.*;
 
 public class SearchMemberByIDFrame {
 
     JFrame smiFrame = new JFrame();
+    CreateLabel label = new CreateLabel("Search Member by ID");
 
     SearchMemberByIDFrame(){
         smiFrame.setSize(800,600);
@@ -13,8 +16,7 @@ public class SearchMemberByIDFrame {
         smiFrame.setLocationRelativeTo(null);
         smiFrame.setTitle("Search Members by ID");
 
-
-
+        smiFrame.add(label);
         smiFrame.setVisible(true);
     }
 }
