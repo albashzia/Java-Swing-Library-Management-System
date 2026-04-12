4e0f9218af05b9a8039920bf6f5e1ef3c791104e
albashzia <ahmedalbashzia@gmail.com>
2026-04-12 17:39:36 +0500

    Defining the attributes of SearchMemberByNameFrame object and making it visible

--- src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByNameFrame.java
+++ src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByNameFrame.java
@@ -7,6 +7,14 @@ public class SearchMemberByNameFrame {
     JFrame smnFrame = new JFrame();
 
     SearchMemberByNameFrame(){
+        smnFrame.setSize(800,600);
+        smnFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        smnFrame.setLayout(null);
+        smnFrame.setLocationRelativeTo(null);
+        smnFrame.setTitle("Search Members by ID");
 
+
+
+        smnFrame.setVisible(true);
     }
 }
