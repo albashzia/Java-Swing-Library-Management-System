328acc48651adac2c61b8d913c025d095e4c16c0
albashzia <ahmedalbashzia@gmail.com>
2026-04-12 18:34:12 +0500

    Creating a backButton object in SearchMemberByNameFrame class

--- src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByNameFrame.java
+++ src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByNameFrame.java
@@ -1,5 +1,6 @@
 package GUI.MemberFrames.SearchMemberFrames;
 
+import GUI.CreateBackButton;
 import GUI.CreateExitButton;
 import GUI.CreateLabel;
 
@@ -10,6 +11,8 @@ public class SearchMemberByNameFrame {
     JFrame smnFrame = new JFrame();
     CreateLabel label = new CreateLabel("Search Member by Name");
     CreateExitButton exitButton = new CreateExitButton();
+    CreateBackButton backButton = new CreateBackButton();
+
     SearchMemberByNameFrame(){
         smnFrame.setSize(800,600);
         smnFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
