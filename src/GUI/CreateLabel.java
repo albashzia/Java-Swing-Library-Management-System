78d5524caf67d2dae654d7193ed26e591ec4d045
albashzia <ahmedalbashzia@gmail.com>
2026-04-14 09:30:58 +0500

    Creating a new constructor in CreateLabel class to accept width along with the title as parameters

--- src/GUI/CreateLabel.java
+++ src/GUI/CreateLabel.java
@@ -17,4 +17,17 @@ public class CreateLabel extends JLabel
         this.setForeground(Color.WHITE);
         this.setOpaque(true);
     }
+
+    public CreateLabel(String title, int width)
+    {
+        this.setLocation(200,30);
+        this.setSize(width,70);
+        this.setBackground(Color.BLACK);
+        this.setHorizontalAlignment(JLabel.CENTER);
+        this.setVerticalAlignment(JLabel.CENTER);
+        this.setText(title);
+        this.setFont(new Font("Comic Sans",Font.ITALIC,35));
+        this.setForeground(Color.WHITE);
+        this.setOpaque(true);
+    }
 }
