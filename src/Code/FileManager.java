31c7c64cdf9517e24a352b5b5e2f958f3471190f
albashzia <ahmedalbashzia@gmail.com>
2026-04-18 09:30:05 +0500

    Creating a file variable

--- src/Code/FileManager.java
+++ src/Code/FileManager.java
@@ -1,8 +1,14 @@
 package Code;
 
+import java.io.File;
+
 public class FileManager {
     static String BOOKS_DATA_FILE = "books_data.txt";
     static String ISSUED_BOOKS_DATA_FILE = "issued_data.txt";
     static String MEMBERS_DATA_FILE = "members.txt";
     static String ROOMS_DATA_FILE = "rooms.txt";
+
+    static void loadBooks(){
+        File file = new File(BOOKS_DATA_FILE);
+    }
 }
