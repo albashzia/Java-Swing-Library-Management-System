97827073ca205d2d593bdc6855ab19a193378774
albashzia <ahmedalbashzia@gmail.com>
2026-04-18 09:26:58 +0500

    Declaring the variables to hold file names

--- src/Code/FileManager.java
+++ src/Code/FileManager.java
@@ -1,4 +1,8 @@
 package Code;
 
 public class FileManager {
+    static String BOOKS_DATA_FILE = "books_data.txt";
+    static String ISSUED_BOOKS_DATA_FILE = "issued_data.txt";
+    static String MEMBERS_DATA_FILE = "members.txt";
+    static String ROOMS_DATA_FILE = "rooms.txt";
 }
