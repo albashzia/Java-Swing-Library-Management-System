b83106c298f74527e959cb6915d15fa035fc8ae7
albashzia <ahmedalbashzia@gmail.com>
2026-04-19 19:21:01 +0500

    Defining a constructor for ArrayListsManager class to initialize the array lists for the Add Book function

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -1,4 +1,12 @@
 package Code;
 
+import java.util.ArrayList;
+
 public class ArrayListsManager {
+    ArrayListsManager(){
+        ArrayList<Integer> bookId = new ArrayList<Integer>();
+        ArrayList<String> bookTitle = new ArrayList<String>();
+        ArrayList<String> bookAuthors = new ArrayList<String>();
+        ArrayList<Integer> booksQuantity = new ArrayList<Integer>();
+    }
 }
