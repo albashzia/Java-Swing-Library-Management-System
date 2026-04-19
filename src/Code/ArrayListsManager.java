3f866b1daba4b9ca79e72791a64c780ae7b6fefe
albashzia <ahmedalbashzia@gmail.com>
2026-04-19 21:27:10 +0500

    Renaming the variable names for 2 array lists

--- src/Code/ArrayListsManager.java
+++ src/Code/ArrayListsManager.java
@@ -3,21 +3,21 @@ package Code;
 import java.util.ArrayList;
 
 public class ArrayListsManager {
-    ArrayList<Integer> bookId;
-    ArrayList<String> bookTitle;
+    ArrayList<Integer> bookIds;
+    ArrayList<String> bookTitles;
     ArrayList<String> bookAuthors;
     ArrayList<Integer> booksQuantity;
 
     public ArrayListsManager(){
-        bookId = new ArrayList<Integer>();
-        bookTitle = new ArrayList<String>();
+        bookIds = new ArrayList<Integer>();
+        bookTitles = new ArrayList<String>();
         bookAuthors = new ArrayList<String>();
         booksQuantity = new ArrayList<Integer>();
     }
 
     public void addBook(int id, String title, String author, int quantity){
-        bookId.add(id);
-        bookTitle.add(title);
+        bookIds.add(id);
+        bookTitles.add(title);
         bookAuthors.add(author);
         booksQuantity.add(quantity);
     }
