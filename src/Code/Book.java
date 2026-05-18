2877992a460c5e6112354ab8d06f9fda821745c7
albashzia <ahmedalbashzia@gmail.com>
2026-05-18 22:53:03 +0500

    Defining the attributes of Book class

--- src/Code/Book.java
+++ src/Code/Book.java
@@ -3,4 +3,8 @@ package Code;
 import java.io.Serializable;
 
 public class Book implements Serializable {
+    private String bookId;
+    private String bookTitle;
+    private String bookAuthor;
+    private int quantity;
 }
