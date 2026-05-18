00b78daf8272d499b5e59eb858b85e7c61fb9dee
albashzia <ahmedalbashzia@gmail.com>
2026-05-18 22:54:35 +0500

    Defining the Parameterized Constructor of Book class

--- src/Code/Book.java
+++ src/Code/Book.java
@@ -7,4 +7,15 @@ public class Book implements Serializable {
     private String bookTitle;
     private String bookAuthor;
     private int quantity;
+
+    public Book(String bookId,
+                String bookTitle,
+                String bookAuthor,
+                int quantity)
+    {
+        this.bookId = bookId;
+        this.bookTitle = bookTitle;
+        this.bookAuthor= bookAuthor;
+        this.quantity = quantity;
+    }
 }
