c024922d303ad97d41877bbc513e2453e5263364
albashzia <ahmedalbashzia@gmail.com>
2026-05-19 23:49:38 +0500

    Defining default constructor of IssuedBook class

--- src/Code/IssuedBook.java
+++ src/Code/IssuedBook.java
@@ -6,4 +6,10 @@ public class IssuedBook implements Serializable {
     Book book;
     Member member;
     private String issueDate;
+
+    IssuedBook(){
+        this.book = null;
+        this.member = null;
+        this.issueDate = null;
+    }
 }
