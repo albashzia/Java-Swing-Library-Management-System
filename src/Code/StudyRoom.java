dc1958c0fdca084953d12b3e343c23a888026454
albashzia <ahmedalbashzia@gmail.com>
2026-05-20 20:03:53 +0500

    Defining the default and parameterized constructor of StudyRoom class

--- src/Code/StudyRoom.java
+++ src/Code/StudyRoom.java
@@ -3,6 +3,18 @@ package Code;
 import java.io.Serializable;
 
 public class StudyRoom implements Serializable {
+    private String roomNo;
     private boolean isReserved;
     private Member reservedByMember;
+
+    StudyRoom(){
+        this.roomNo = null;
+        this.isReserved = false;
+        this.reservedByMember = null;
+    }
+
+    StudyRoom(String roomNo, Member reservedByMember){
+        this.roomNo = roomNo;
+        this.reservedByMember = reservedByMember;
+    }
 }
