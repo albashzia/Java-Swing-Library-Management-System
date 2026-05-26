2ed7364f82434778100971a496aaa0dad7069b24
albashzia <ahmedalbashzia@gmail.com>
2026-05-26 21:17:12 +0500

    Defining the constructor of CreateFrameButton class

--- src/GUI/AdditionalClasses/CreateFrameButton.java
+++ src/GUI/AdditionalClasses/CreateFrameButton.java
@@ -1,4 +1,17 @@
 package GUI.AdditionalClasses;
 
-public class CreateFrameButton {
+import javax.swing.*;
+import java.awt.*;
+
+public class CreateFrameButton extends JButton {
+
+    CreateFrameButton(String text,int x, int y){
+        this.setText(text);
+        this.setSize(200,80);
+        this.setLocation(x,y);
+        this.setFocusable(false);
+        this.setFont(new Font("Canvas",Font.BOLD,20));
+        this.setBackground(Color.BLACK);
+        this.setForeground(Color.WHITE);
+    }
 }
