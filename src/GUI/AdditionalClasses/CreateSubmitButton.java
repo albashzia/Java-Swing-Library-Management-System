33b8abebcea7d9265757b5b8a2f2395b804d8673
albashzia <ahmedalbashzia@gmail.com>
2026-05-28 19:39:21 +0500

    Defining the background and foreground color of Submit Button in CreateSubmitButton class

--- src/GUI/AdditionalClasses/CreateSubmitButton.java
+++ src/GUI/AdditionalClasses/CreateSubmitButton.java
@@ -1,21 +1,28 @@
 package GUI.AdditionalClasses;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CreateSubmitButton extends JButton {
     public CreateSubmitButton(String text){
+        this.setBackground(Color.BLACK);
+        this.setForeground(Color.WHITE);
         this.setBounds(350,330,100,50);
         this.setText(text);
         this.setFocusable(false);
     }
 
     public CreateSubmitButton(int x, int y, String text){
+        this.setBackground(Color.BLACK);
+        this.setForeground(Color.WHITE);
         this.setBounds(x,y,100,50);
         this.setText(text);
         this.setFocusable(false);
     }
 
     public CreateSubmitButton(int x, int y, int width, int height,  String text){
+        this.setBackground(Color.BLACK);
+        this.setForeground(Color.WHITE);
         this.setBounds(x,y,width,height);
         this.setText(text);
         this.setFocusable(false);
