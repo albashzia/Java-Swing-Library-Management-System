9fe6f5c4c27933ce5c05ff7c7bb45c207f206409
albashzia <ahmedalbashzia@gmail.com>
2026-05-31 22:16:45 +0500

    Setting the relative position and Size of CreateDialogBox class

--- src/GUI/AdditionalClasses/CreateDialogBox.java
+++ src/GUI/AdditionalClasses/CreateDialogBox.java
@@ -6,6 +6,8 @@ public class CreateDialogBox extends JDialog {
 
     public CreateDialogBox(String title, String text){
         this.setTitle(title);
+        this.setLocationRelativeTo(null);
+        this.setSize(200,100);
         JLabel message = new JLabel(text);
         this.add(message);
         this.setVisible(true);
