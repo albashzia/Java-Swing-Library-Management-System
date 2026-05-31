fdf058a632b29945ac52fc7001a9027a62063439
albashzia <ahmedalbashzia@gmail.com>
2026-05-31 22:08:01 +0500

    Defining the basic properties of the CreateDialogBox class

--- src/GUI/AdditionalClasses/CreateDialogBox.java
+++ src/GUI/AdditionalClasses/CreateDialogBox.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class CreateDialogBox extends JDialog {
 
     public CreateDialogBox(String title, String text){
-
+        this.setTitle(title);
+        JTextField message = new JTextField(text);
+        this.add(message);
     }
 }
