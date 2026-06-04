71f425cf65bcaf516fe324183153c95367b39ed5
albashzia <ahmedalbashzia@gmail.com>
2026-06-04 13:44:53 +0500

    Changing the size and position of idTextField in ReturnBookFrame class

--- src/GUI/BookFrames/ReturnBookFrame.java
+++ src/GUI/BookFrames/ReturnBookFrame.java
@@ -34,7 +34,7 @@ public class ReturnBookFrame implements ActionListener {
         idLabel.setBounds(10,80,200,100);
         idLabel.setFont(new Font("Comic Sans",Font.PLAIN,25));
 
-        idTextField.setBounds(350,170,100,40);
+        idTextField.setBounds(10,150,760,30);
         idTextField.setFont(new Font("Comic",Font.BOLD,20));
         idTextField.setForeground(Color.WHITE);
         idTextField.setBackground(Color.BLACK);
