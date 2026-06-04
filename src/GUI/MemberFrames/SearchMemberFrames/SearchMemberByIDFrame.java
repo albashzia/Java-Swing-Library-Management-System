160873095ec0a2fe41b5f5eb6dc2bed0ed5ad782
albashzia <ahmedalbashzia@gmail.com>
2026-06-04 13:49:10 +0500

    Changing the size and position of idTextField in SearchMemberByIDFrame class

--- src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
+++ src/GUI/MemberFrames/SearchMemberFrames/SearchMemberByIDFrame.java
@@ -31,7 +31,7 @@ public class SearchMemberByIDFrame implements ActionListener {
         idLabel.setBounds(10,150,200,100);
         idLabel.setFont(new Font("Cosmic Sans",Font.PLAIN,20));
 
-        idTextField.setBounds(270,210,100,30);
+        idTextField.setBounds(10,220,760,30);
         idTextField.setFont(new Font("Consolas",Font.BOLD,20));
         idTextField.setForeground(Color.WHITE);
         idTextField.setBackground(Color.BLACK);
