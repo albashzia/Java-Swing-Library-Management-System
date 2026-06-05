e3d800d611789053d8358dc79333eec7a1b6f638
albashzia <ahmedalbashzia@gmail.com>
2026-06-05 19:46:21 +0500

    Creating a JLabel object and adding it to the IssuedToMemberFrame

--- src/GUI/MemberFrames/IssuedToMemberFrame.java
+++ src/GUI/MemberFrames/IssuedToMemberFrame.java
@@ -5,15 +5,21 @@ import GUI.AdditionalClasses.CreateExitButton;
 import GUI.AdditionalClasses.CreateLabel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class IssuedToMemberFrame implements ActionListener {
     JFrame itmFrame = new JFrame();
+    JLabel idLabel = new JLabel();
     CreateBackButton backButton = new CreateBackButton();
     CreateExitButton exitButton = new CreateExitButton();
     CreateLabel label1 = new CreateLabel("Books Issued to Member");
     IssuedToMemberFrame(){
+
+        idLabel.setText("Enter Member ID : ");
+        idLabel.setBounds(10,80,200,100);
+        idLabel.setFont(new Font("Cosmic Sans",Font.PLAIN,20));
 
         itmFrame.setSize(800,600);
         itmFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,6 +29,7 @@ public class IssuedToMemberFrame implements ActionListener {
 
         backButton.addActionListener(this);
 
+        itmFrame.add(idLabel);
         itmFrame.add(exitButton);
         itmFrame.add(label1);
         itmFrame.add(backButton);
