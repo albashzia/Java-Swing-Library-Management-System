833bfb7f5c23383ddd8059f8f449ee2253a5798f
albashzia <ahmedalbashzia@gmail.com>
2026-07-05 23:18:12 +0500

    Adding the removeBook method in BooksService class

--- src/Service/BooksService.java
+++ src/Service/BooksService.java
@@ -4,13 +4,28 @@ import Code.ArrayListsManager;
 import Code.Book;
 import GUI.AdditionalClasses.CreateDialogBox;
 
+import java.util.ArrayList;
+
 public class BooksService {
 
     ArrayListsManager manager = ArrayListsManager.instance;
+    ArrayList<Book> bookArrayList = manager.returnBooksArrayList();
 
     public void addBook(String id,String name,String author,int quantity){
         Book book = new Book(id,name,author,quantity);
         manager.addBook(book);
         CreateDialogBox dialogBox = new CreateDialogBox("Success", "Book Added Successfully");
+    }
+
+    public void removeBook(String id){
+        Book removableBook = null;
+        for (int i = 0; i < bookArrayList.size(); i++) {
+            if (bookArrayList.get(i).getId().equalsIgnoreCase(id)){
+                removableBook = bookArrayList.get(i);
+            }
+        }
+        if(removableBook!=null){
+            manager.removeBookById(removableBook);
+        }
     }
 }
