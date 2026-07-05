e9483654e0b32dc6132b3bf197c5a6f4863b1ca6
albashzia <ahmedalbashzia@gmail.com>
2026-07-05 23:01:58 +0500

    Creating an instance of ArrayListsManager class in BooksService

--- src/Service/BooksService.java
+++ src/Service/BooksService.java
@@ -1,4 +1,11 @@
 package Service;
 
+import Code.ArrayListsManager;
+import Code.Book;
+import GUI.AdditionalClasses.CreateDialogBox;
+
 public class BooksService {
+
+    ArrayListsManager manager = ArrayListsManager.instance;
+
 }
