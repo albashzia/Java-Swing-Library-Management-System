f725fc2b322e7326b5bc999468bfcb141eaadb6c
albashzia <ahmedalbashzia@gmail.com>
2026-07-05 23:02:52 +0500

    Creating a method named addBook in BooksService class to receive parameters from GUI, create a Book object, adding it to the ArrayList and displaying the dialog box

--- src/Service/BooksService.java
+++ src/Service/BooksService.java
@@ -8,4 +8,9 @@ public class BooksService {
 
     ArrayListsManager manager = ArrayListsManager.instance;
 
+    public void addBook(String id,String name,String author,int quantity){
+        Book book = new Book(id,name,author,quantity);
+        manager.addBook(book);
+        CreateDialogBox dialogBox = new CreateDialogBox("Success", "Book Added Successfully");
+    }
 }
